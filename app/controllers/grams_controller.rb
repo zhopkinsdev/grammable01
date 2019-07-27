f4f23f29535eab6a32d2974b61f9beb9d1bfc30d
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-27 20:55:45 +0000

    adding factories

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,7 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  
   def new
     @gram = Gram.new
   end
@@ -7,8 +10,12 @@ class GramsController < ApplicationController
   end
 
   def create
-    @gram = Gram.create(gram_params)
-    redirect_to root_path
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
   root "grams#index"
     resources :grams, only: [:new, :create]
 end
