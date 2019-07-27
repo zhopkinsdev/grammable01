2bd3d22d60a67d0a7969d3373739f7cdeb17f51c
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-27 06:39:03 +0000

    devise attempt no.1

--- Gemfile
+++ Gemfile
@@ -69,6 +69,7 @@ end
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'simple_form'
+gem 'devise'
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   root "grams#index"
     resources :grams, only: [:new, :create]
 end
