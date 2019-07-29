fb11b8f285316372a6f8251dab3ef39881b2bbe7
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-29 03:49:33 +0000

    adding gram details

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,12 +1,19 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  
+
   def new
     @gram = Gram.new
   end
 
   def index
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'That Gram Not Found :(', status: :not_found
+    end
   end
 
   def create
